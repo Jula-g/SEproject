1e00852e3b0285ddd4a56c1f07c3395a7edc9192
Damian Stępień <151776252+damianSte@users.noreply.github.com>
2024-11-27 10:53:21 +0100

    updated version logIn and Register Actvity, firebase works.

---
+++ PhysioConsult/app/src/main/java/com/example/physioconsult/firebase/User.kt
@@ -0,0 +1,8 @@
+package com.example.physioconsult.firebase
+
+
+class User( val id: String="",
+            val name: String="",
+            val email: String="",
+            val registeredUser: Boolean = false,
+)
