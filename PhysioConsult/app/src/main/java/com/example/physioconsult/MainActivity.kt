cb53cb3507276994bb7396f6eedff7bce0fe528d
Justy <justynaonceagainforgothermail@gmail.com>
2024-12-02 14:06:58 +0100

    Task bar and button initialisation on landing page, welcoming the user (without patient name due to Db problem)

--- PhysioConsult/app/src/main/java/com/example/physioconsult/MainActivity.kt
+++ PhysioConsult/app/src/main/java/com/example/physioconsult/MainActivity.kt
@@ -2,17 +2,16 @@ package com.example.physioconsult
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
-
+import androidx.activity.compose.setContent
+import com.example.physioconsult.ui.theme.PhysioConsultTheme
 
 class MainActivity : ComponentActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setContent {
+            PhysioConsultTheme {
+                MainActivityForm()
+            }
         }
     }
-
-
+}
