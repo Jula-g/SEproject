0c24874647d56a84444f7568e1183ea76b1d76bc
Julia Galant <276068@student.pwr.edu.pl>
2025-01-06 17:14:32 +0100

    Physiotherapist main view created, dark/light mode colors change updated.

---
+++ PhysioConsult/app/src/main/java/com/example/physioconsult/physiotherapist/PhysiotherapistForm.kt
@@ -0,0 +1,182 @@
+package com.example.physioconsult.physiotherapist
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Assignment
+import androidx.compose.material.icons.filled.AssignmentTurnedIn
+import androidx.compose.material.icons.filled.ContentPaste
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PeopleOutline
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.physioconsult.Main.CardButtonAppointments
+import com.example.physioconsult.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun PhysiotherapistForm() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val colors = MaterialTheme.colorScheme
+    val auth = FirebaseAuth.getInstance()
+    val userId = auth.currentUser?.uid
+    val name = remember { mutableStateOf("Name") }
+    val surname = remember { mutableStateOf("Surname") }
+
+    if (userId != null) {
+        val firestore = FirebaseFirestore.getInstance()
+        firestore.collection("users").document(userId)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    name.value = document.getString("name") ?: "User"
+                    surname.value = document.getString("surname") ?: "Surname"
+                }
+            }
+            .addOnFailureListener {
+                Log.e("MainActivityForm", "Failed to get user data", it)
+            }
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(75.dp)
+                    ) {
+                        Text("PhysioConsult", color = Color(0xFF84ACD8))
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu Icon",
+                            tint = Color(0xFF84ACD8)
+                        )
+                    }
+                },
+            )
+        },
+        bottomBar = {
+            BottomNavPhysiotherapist()
+        }
+    ) { innerPadding ->
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+                .verticalScroll(scrollState),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text(
+                        text = "Hello!",
+                        fontSize = 28.sp,
+                        color = colors.onSurface
+                    )
+                    Text(
+                        text = "Dr ${name.value} ${surname.value}",
+                        fontSize = 20.sp,
+                        color = colors.onSurface
+                    )
+                }
+
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "User Avatar",
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.LightGray, RoundedCornerShape(36.dp))
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Welcome!",
+                fontSize = 24.sp,
+                color = colors.onSurface,
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Start)
+            )
+            Text(
+                text = "Physio Consult is an app that speeds up the process of physical body measurements, and assessments of it measurement.",
+                fontSize = 14.sp,
+                color = colors.onSurface,
+                textAlign = TextAlign.Start
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            val imageChart: Painter =
+                painterResource(id = R.drawable.medical)
+            CardButtonAppointments(
+                text = "View Appointments",
+                title = "Appointments",
+                description = "View all your appointments",
+                icon = imageChart,
+                backgroundColor = Color(0xFF84ACD8)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            CardButton(
+                text = "Retrieve Assessments",
+                title = "Assessments",
+                description = "Get an assessment from the patient",
+                icon = Icons.Default.ContentPaste,
+                backgroundColor = Color(0xFF84ACD8),
+                iconTint = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            CardButton(
+                text = "View Patients",
+                title = "Patients",
+                description = "View the list of all your patients along with their assessments",
+                icon = Icons.Default.PeopleOutline,
+                backgroundColor = Color(0xFF84ACD8),
+                iconTint = Color.White
+            )
+        }
+    }
+}
